c8eaeafb9eadc3c3d6a9df1b7468750d73c0467c
Joey Patino <joseph.onitap@gmail.com>
2019-11-27 13:48:24 +0100

    Initial Commit

---
+++ TextAttribute/TextAttribute.h
@@ -0,0 +1,19 @@
+//
+//  TextAttribute.h
+//  TextAttribute
+//
+//  Created by Joseph Patino on 11/27/19.
+//  Copyright © 2019 Joseph Patino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextAttribute.
+FOUNDATION_EXPORT double TextAttributeVersionNumber;
+
+//! Project version string for TextAttribute.
+FOUNDATION_EXPORT const unsigned char TextAttributeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextAttribute/PublicHeader.h>
+
+
